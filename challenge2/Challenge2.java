d5dcf882e2fbcb5d89e1ba76174c6ac10800339d
Nolan Platt <43970139+nolanplatt@users.noreply.github.com>
2022-03-29 00:00:51 -0700

    (+) Calculator finished w/ all operators, including EC SQRT()
    (+)
    - All operators working according to PEMDAS, with SQRT() extra credit functionality working
    - Try/catch in input scanner to catch user errors
    - User choosing test data or user-inputted data in console scanner

--- challenge2/Challenge2.java
+++ challenge2/Challenge2.java
@@ -14,22 +14,37 @@ public class Challenge2 {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);  
 
+        System.out.println("-------------------------\n");
+        System.out.println("Data Structures  Challenge 2");
+        System.out.println("Select an Option...");
+        System.out.println("1 - No Input (Test Data)");
+        System.out.println("2 - User Input (Your Data)");
+        System.out.println("-------------------------\n");
 
+        
+        int option;
+        option = scan.nextInt();  
+        if(option == 1) {
+            System.out.println("TESTING: ( 3 + SQRT 2 * 5^3 )");
+            Calculator calculator = new Calculator(" ( 3 + SQRT 2 * 5^3 )");
+            System.out.println(calculator.toString());
+        } else {
         System.out.println("-------------------------\n");
         System.out.println("Calculator: Awaiting input");
-        System.out.println("Please include paranthesis () ");
+        System.out.println("Please include parenthesis () ");
         System.out.println("-------------------------\n");
 
+        Scanner scan2 = new Scanner(System.in);  
 
         String input;
         try {
-            input = scan.nextLine();  
+            input = scan2.nextLine();  
             Calculator calculator = new Calculator(input);
             System.out.println(calculator.toString());
         } catch (Exception error) {
-            System.out.println("Make sure to include paranthesis next time.");
+            System.out.println("Make sure to include parenthesis next time.");
             System.exit(0);
         }
-      
-    }
+        } 
+       }
 }
