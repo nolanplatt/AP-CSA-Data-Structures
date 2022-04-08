28948865e8ac7ea5cae3c7e4ec805699f89093f8
Caleb Kim <89210382+calebkimsd@users.noreply.github.com>
2022-04-07 22:42:07 -0700

    Add new Swap to code

--- challenge0/imports/Swap.java
+++ challenge0/imports/Swap.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class Swap {
 
     public Swap() {
+        
+         //print a ver of swap that display without using input/output
+        System.out.println("Orginial Set: 16, 5");
+       int option3 = 5;  
+        int option4 = 16;  
+
+        if(option3 > option4) {
+            int swap = option3;
+            option3 = option4;
+            option4 = swap;
+        }
+        System.out.println("Swapped Set: " + option3 + ", " + option4);
+        
         Scanner scan = new Scanner(System.in);
         System.out.println("-------------------------\n");
         System.out.println("Need 2 numbers...");
